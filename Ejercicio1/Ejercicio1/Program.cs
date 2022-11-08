d2536f949b77a01f99b392f4a57fbd20e75334ca
Andres Licona <105387601+andreslicona27@users.noreply.github.com>
2022-11-08 19:46:15 +0100

    Add files via upload

---
+++ Ejercicio1/Ejercicio1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection.Emit;
+
+namespace Ejercicio1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int[] v = { 2, 2, 6, 7, 1, 10, 3 };
+
+            Array.ForEach(v, (item) =>
+            {
+                Console.ForegroundColor = item >= 5 ? ConsoleColor.Green : ConsoleColor.Red;
+                Console.WriteLine($"Student grade: {item,3}.");
+                Console.ResetColor();
+            });
+            int res = Array.FindIndex(v, (item) => item >= 5);
+
+
+            Console.WriteLine("There's one approved");
+            Console.WriteLine(Array.Exists(v, item => item > 4));
+
+            Console.WriteLine("The position of the last one approved");
+            Console.WriteLine(Array.FindLastIndex(v, item => item >= 5));
+
+            Console.WriteLine("Inverse of all the grades");
+            Array.ForEach(v, item => Console.WriteLine(1.0 / item));
+            
+
+        }
+    }
+}
---
+++ Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,79 @@
+﻿using static Ejercicio2.Program;
+
+namespace Ejercicio2
+{
+    internal class Program
+    {
+        public delegate void MyDelegate();
+
+        public static bool MenuGenerator(string[] options, MyDelegate[] functions)
+        {
+            int opcCont = 1;
+            string opcMenu;
+            int result;
+      
+            if (options.Length != functions.Length || (options == null || functions == null))
+            {
+                Console.WriteLine("There it's an error in the parameters.");
+                return false;
+            }
+            else
+            {
+                do
+                {
+                    opcCont = 1;
+                    Console.WriteLine("\nOPTIONS MENU");
+                    Array.ForEach(options, item => Console.WriteLine("{0}) {1}", opcCont++, item));
+                    Console.WriteLine("{0}) Exit", opcCont);
+                    opcMenu = Console.ReadLine();
+                    
+                    if(int.TryParse(opcMenu, out result))
+                    {
+                        result = int.Parse(opcMenu);
+
+                        if (result < options.Length + 1 && result >= 1)
+                        {
+                            functions[result - 1]();
+                        } 
+                        else if (result != options.Length + 1)
+                        {
+                            Console.WriteLine("That option is out of range, try again.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("The option has to be a number.");
+                    }
+
+
+                } while (result != options.Length + 1);
+                return true;
+            }
+
+        }
+        //static void f1()
+        //{
+        //    Console.WriteLine("A");
+        //}
+        //static void f2()
+        //{
+        //    Console.WriteLine("B");
+        //}
+        //static void f3()
+        //{
+        //    Console.WriteLine("C");
+        //}
+        static void Main(string[] args)
+        {
+            MenuGenerator(new string[] { "Op1", "Op2", "Op3" }, 
+                new MyDelegate[] { 
+                    () => Console.WriteLine("A"), 
+                    () => Console.WriteLine("B"), 
+                    () => Console.WriteLine("C") });
+            Console.ReadKey();
+        }
+
+
+
+    }
+}

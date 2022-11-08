64f63dcab87aa39682fc7ae838ad24a3f5fe7d48
Andres Licona <105387601+andreslicona27@users.noreply.github.com>
2022-11-08 19:48:56 +0100

    Add files via upload

---
+++ Ejercicio5/Ejercicio5/Horse.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5
+{
+    internal class Horse
+    {
+        static readonly object l = new object();
+        static bool finished = false;
+        private int position;
+        public int Position
+        {
+            set { position = value; }
+            get { return position; }
+        }
+
+        public void Run()
+        {
+            while (!finished)
+            {
+                lock (l)
+                {
+                    if (!finished)
+                    {
+                        for (int i = 0; i < 40; i++)
+                        {
+                            Console.WriteLine("*");
+                        }
+                        
+                        if (position == 40)
+                        {
+                            finished = true;
+                        }
+                        
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Ejercicio5/Ejercicio5/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Ejercicio5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Horse[] horses = new Horse[5];
+            Thread[] threads = new Thread[5];
+            bool win = false;
+            int bet;
+
+            horses[0] = new Horse();
+            horses[1] = new Horse();
+            horses[2] = new Horse();
+            horses[3] = new Horse();
+            horses[4] = new Horse();
+
+            horses[0].Position = 0;
+            horses[1].Position = 0;
+            horses[2].Position = 0;
+            horses[3].Position = 0;
+            horses[4].Position = 0;
+
+
+            do
+            {
+                Console.WriteLine("HORSES\n1)Freud\n2)Kahneman\n3)Peterson\n4)Grant\n5)Goleman\nWhat horse do you bet on?");
+                bet = int.Parse(Console.ReadLine());
+
+                while (bet < 1 || bet > 5)
+                {
+                    Console.WriteLine("Bet only for horses on the list");
+                    Console.WriteLine("HORSES\n1)Freud\n2)Kahneman\n3)Peterson\n4)Grant\n5)Goleman\nWhat horse do you bet on?");
+                    bet = int.Parse(Console.ReadLine());
+                }
+
+                for (int i =0; i < horses.Length; i++)
+                {
+                    threads[i] = new Thread(horses[i].Run);
+                    threads[i].Start();
+                }
+
+                Array.ForEach(threads, item => item.Join());
+                
+
+            } while (win);
+            
+
+
+        }
+    }
+}

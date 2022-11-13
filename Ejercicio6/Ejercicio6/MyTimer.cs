a3df1270237fad49e9ad9e2e8331d486624b6c01
Andres Licona <andreslicona27@icloud.com>
2022-11-13 20:08:26 +0100

    creacion de E6

---
+++ Ejercicio6/Ejercicio6/MyTimer.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio6
+{
+    public delegate void MyDelegate();
+
+    internal class MyTimer
+    {
+        Thread thread;
+        MyDelegate function;
+
+        static readonly private object l = new object();
+        private static bool isRunning = false;
+        private static bool isPaused = false;
+        public int interval;
+
+        // BUILDER 
+        public MyTimer(MyDelegate function)
+        {
+            this.function = new MyDelegate(function);
+        }
+
+        // FUNCTIONS
+        public void run()
+        {
+            while (!isRunning)
+            {
+                lock (l)
+                {
+                    if (!isRunning)
+                    {
+                        isRunning = true;
+                        thread = new Thread(printNumbers);
+                        thread.Start();
+                    }
+                    else
+                    {
+                        isPaused = true;
+                        Monitor.Pulse(l);
+                    }
+                }
+
+            }
+        }
+
+        public void pause()
+        {
+            while (!isPaused)
+            {
+                lock (l)
+                {
+                    isPaused = true;
+                }
+            }
+        }
+
+        public void printNumbers()
+        {
+            while (!isPaused)
+            {
+                function.Invoke();
+                Thread.Sleep(interval);
+                lock(l)
+                {
+                    if(isPaused)
+                    {
+                        Monitor.Wait(l);
+                    }
+                }
+            }
+        }
+
+    }
+}

eb774d8c03b8b8adf22fb16037f4901bc17b2382
Andres Licona <andreslicona27@icloud.com>
2022-12-03 19:09:50 +0100

    E7 done

--- Ejercicio7/Ejercicio7/Program.cs
+++ Ejercicio7/Ejercicio7/Program.cs
@@ -1,19 +1,22 @@
 ﻿
+using System;
+
 namespace Ejercicio7
 {
     internal class Program
     {
         private static readonly object l = new object();
+        public static Random ran = new Random();
+        static int comunCont = 0;
         public static bool gameFinish = false;
         public static bool animationPlaying = true;
         public static bool firstround = false;
-        public static Random ran = new Random();
-        static int comunCont = 0;
 
 
         public static void Player1Function()
         {
             int num = 0;
+            int timeSleeping = 0;
             while (!gameFinish)
             {
                 lock (l)
@@ -21,37 +24,42 @@ namespace Ejercicio7
                     if (!gameFinish)
                     {
                         num = ran.Next(1, 11);
+                        timeSleeping = 100 * num;
+
                         Console.SetCursorPosition(2, 4);
-                        Console.Write("PLAYER one: {0,2}", num);
+                        Console.WriteLine("PLAYER one: {0,2}", num);
+
                         if (num == 5 || num == 7)
                         {
-                            if (!animationPlaying)
-                            {
-                                Monitor.Wait(l);
-                                animationPlaying = false;
-                                comunCont++;
-                                PrintNumbers(true, false);
-                            }
-                            else
-                            {
-                                comunCont += 5;
-                                PrintNumbers(true, true);
-                            }
-
                             if (comunCont >= 20 || comunCont <= -20)
                             {
                                 gameFinish = true;
                             }
+                            else
+                            {
+                                if (animationPlaying)
+                                {
+                                    animationPlaying = false;
+                                    comunCont++;
+                                    PrintNumbers(true, false);
+                                }
+                                else
+                                {
+                                    comunCont += 5;
+                                    PrintNumbers(true, true);
+                                }
+                            }
                         }
                     }
                 }
-                Thread.Sleep(ran.Next(100, num * 100));
+                Thread.Sleep(ran.Next(100, timeSleeping));
             }
         }
 
         public static void Player2Function()
         {
             int num = 0;
+            int timeSleeping = 0;
             while (!gameFinish)
             {
                 lock (l)
@@ -59,35 +67,40 @@ namespace Ejercicio7
                     if (!gameFinish)
                     {
                         num = ran.Next(1, 11);
+                        timeSleeping = 100 * num;
+
                         Console.SetCursorPosition(2, 5);
-                        Console.Write("PLAYER two: {0,2}", num);
+                        Console.WriteLine("PLAYER two: {0,2}", num);
+
                         if (num == 5 || num == 7)
                         {
-                            if (!animationPlaying)
-                            {
-                                Monitor.Pulse(l);
-                                animationPlaying = true;
-                                comunCont--;
-                                PrintNumbers(false, false);
-                            }
-                            else
-                            {
-                                if (!firstround)
-                                {
-                                    comunCont -= 5;
-                                    PrintNumbers(false, true);
-                                }
-                                firstround = false;
-                            }
-
                             if (comunCont >= 20 || comunCont <= -20)
                             {
                                 gameFinish = true;
                             }
+                            else
+                            {
+                                if (!animationPlaying)
+                                {
+                                    animationPlaying = true;
+                                    comunCont--;
+                                    PrintNumbers(false, false);
+                                    Monitor.Pulse(l);
+                                }
+                                else
+                                {
+                                    if (!firstround)
+                                    {
+                                        comunCont -= 5;
+                                        PrintNumbers(false, true);
+                                    }
+                                    firstround = false;
+                                }
+                            }
                         }
                     }
                 }
-                Thread.Sleep(ran.Next(100, num * 100));
+                Thread.Sleep(ran.Next(100, timeSleeping));
             }
         }
 
@@ -130,6 +143,7 @@ namespace Ejercicio7
                             //RIGHT
                             Console.SetCursorPosition(20, j);
                             Console.WriteLine(displayIcon[cont]);
+
                             if (j == 6)
                             {
                                 j = 2;
@@ -139,7 +153,6 @@ namespace Ejercicio7
                                 j++;
                             }
                         }
-
                     }
                 }
                 Thread.Sleep(200);
@@ -151,32 +164,24 @@ namespace Ejercicio7
             // bool player1 is for identify the player and placing the cursor in the right position
             // bool getlucky is to know if the random number is 5 or 7 so we can print +5 if not we print +1
             Console.SetCursorPosition(2, 3);
-            Console.WriteLine("COMUN NUMBER: " + comunCont);
+            Console.WriteLine("COMUN NUMBER: {0,3}", comunCont);
+
             if (player1)
             {
                 Console.SetCursorPosition(17, 4);
-                if (getLucky)
-                {
-                    Console.WriteLine("+5");
-                }
-                else
-                {
-                    Console.WriteLine("+1");
-                }
-                Console.WriteLine("  ");
             }
             else
             {
                 Console.SetCursorPosition(17, 5);
-                if (getLucky)
-                {
-                    Console.WriteLine("+5");
-                }
-                else
-                {
-                    Console.WriteLine("+1");
-                }
-                Console.WriteLine("  ");
+            }
+
+            if (getLucky)
+            {
+                Console.Write("+5");
+            }
+            else
+            {
+                Console.Write("+1");
             }
         }
 
@@ -195,10 +200,12 @@ namespace Ejercicio7
             player2.Join();
             display.Join();
 
-            if (comunCont == 20)
+            Console.SetCursorPosition(1, 8);
+            if (comunCont >= 20)
             {
                 Console.WriteLine("Player One Wins");
-            } else
+            }
+            else
             {
                 Console.WriteLine("Player Two Wins");
             }

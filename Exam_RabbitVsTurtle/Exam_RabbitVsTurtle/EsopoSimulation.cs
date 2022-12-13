952a0fa964fcf557c097f5618ac8cae257aac22b
Andres Licona <andreslicona27@icloud.com>
2022-12-13 21:42:04 +0100

    exam finish

--- Exam_RabbitVsTurtle/Exam_RabbitVsTurtle/EsopoSimulation.cs
+++ Exam_RabbitVsTurtle/Exam_RabbitVsTurtle/EsopoSimulation.cs
@@ -9,17 +9,27 @@ namespace Exam_RabbitVsTurtle
     internal class EsopoSimulation
     {
         // PROPERTIES
-        public object l = new object();
-        bool raceFinish = false;
+        private readonly object l = new object();
+        Random rand = new Random();
+
         int turtleSteps = 0;
         int rabbitSteps = 0;
         int finishLine = 50;
+        bool raceFinish = false;
+        bool sleeping;
+
 
 
 
         // FUNCITONS
         public string Init()
         {
+            // LEER DATO DE UN ARCHIVO BINARIO - faltaría agregar el try catch
+            //using (BinaryReader br = new BinaryReader(new FileStream(Environment.GetEnvironmentVariable("userprofile") + "\\meta.bin", FileMode.Open)))
+            //{
+            //    finishLine = br.ReadInt32();
+            //}
+
             Thread turtle = new Thread(RunTurtleRun);
             Thread rabbit = new Thread(RunAndSleepRabbit);
 
@@ -52,6 +62,16 @@ namespace Exam_RabbitVsTurtle
                         turtleSteps++;
                         Console.WriteLine($"Leonardo move {turtleSteps} steps");
 
+                        if (turtleSteps == rabbitSteps && sleeping)
+                        {
+                            int num = rand.Next(0, 100);
+                            if (num < 50)
+                            {
+                                Console.WriteLine("Leonardo makes noise");
+                                Monitor.Pulse(l);
+                            }
+                        }
+
                         if (turtleSteps >= finishLine || rabbitSteps >= finishLine)
                         {
                             raceFinish = true;
@@ -77,42 +97,38 @@ namespace Exam_RabbitVsTurtle
                         {
                             raceFinish = true;
                         }
-                        //if (SweetDreams(rabbitSteps))
-                        //{
-                        //    Console.WriteLine("Seems like Bugs thinks is good time to take a nap");
-                        //}
+                        else
+                        {
+                            if (rand.Next(1, 101) <= 60)
+                            {
+                                int auxSteps = rabbitSteps;
+                                Console.WriteLine($"Seems like Bugs thinks is good time to take a nap when he made {rabbitSteps} steps");
+                                
+                                Thread dreams = new Thread(() => SweetDreams(auxSteps));
+                                dreams.Start();
+                                Monitor.Wait(l);
+
+                                Console.WriteLine($"Bugs Bunny finally wakes up at {rabbitSteps} steps");
+                            }
+                        }
                     }
                 }
                 Thread.Sleep(200);
             }
         }
 
-        public bool SweetDreams(int currentsteps)
+        public void SweetDreams(int currentsteps)
         {
-            bool sleeping = false;
-            Random rand = new Random();
-            while (!raceFinish)
+            Thread.Sleep(2500);
+            lock (l)
             {
-                lock (l)
+                if(currentsteps == rabbitSteps)
                 {
-                    if (!raceFinish)
-                    {
-                        int num = rand.Next(0, 100);
-                        if (num < 60)
-                        {
-                            Thread.Sleep(2500);
-                            sleeping = true;
-                        }
-                        else
-                        {
-                            sleeping = false;
-                        }
-
-                    }
+                    Monitor.Pulse(l);
                 }
             }
-            return sleeping;
-
         }
+
     }
+}
 }
